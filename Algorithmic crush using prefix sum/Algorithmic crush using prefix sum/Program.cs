26b93a477cd845948a9e0996c841040e99d1e7e2
Omar Meriwani <omar.sirwan@gmail.com>
2017-05-15 02:19:07 +0300

    --

---
+++ Algorithmic crush using prefix sum/Algorithmic crush using prefix sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Algorithmic_crush_using_prefix_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10000000 100000
+            //string[] temp1 = (Console.ReadLine().Split(' '));
+            Int64 n = 10000000; //Convert.ToInt64(temp1[0]);
+            Int64 m = 100000; //Convert.ToInt64(temp1[1]);
+            Int64[] resultArr = new Int64[n];
+
+            StreamReader sr = new StreamReader(@"C:\Users\Omar\AppData\Local\Temporary Projects\Algorithmic crush using prefix sum\Algorithmic Crush input 13.txt");
+            string line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                Int64[] a = new Int64[3];
+                a = Array.ConvertAll(line.Split(' '), Int64.Parse);
+                resultArr[a[0] - 1] += a[2];
+                if ((a[1]) < n)
+                    resultArr[a[1]] -= a[2];
+            }
+
+            //for (Int64 i = 0; i < m; i++)
+            //{
+            //    Int64[] a = new Int64[n];
+            //    a = Array.ConvertAll(Console.ReadLine().Split(' '), Int64.Parse);
+            //    resultArr[a[0] - 1] += a[2];
+            //    if ((a[1]  ) < n)
+            //        resultArr[a[1] ] -= a[2];
+            //}
+            Int64 max = resultArr[0];
+            Int64 sum = resultArr[0];
+            for (int i = 1; i < n; i++)
+            {
+                sum += resultArr[i];
+                if (max < sum)
+                    max = sum;
+            }
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithmic crush using prefix sum/Algorithmic crush using prefix sum/withoutInput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithmic_crush_using_prefix_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] temp1 = (Console.ReadLine().Split(' '));
+            Int64 n = Convert.ToInt64(temp1[0]);
+            Int64 m = Convert.ToInt64(temp1[1]);
+            Int64[] resultArr = new Int64[n];
+
+
+
+            for (Int64 i = 0; i < m; i++)
+            {
+                Int64[] a = new Int64[3];
+                a = Array.ConvertAll(Console.ReadLine().Split(' '), Int64.Parse);
+                resultArr[a[0] - 1] += a[2];
+                if ((a[1]) < n)
+                    resultArr[a[1]] -= a[2];
+            }
+            Int64 max = resultArr[0];
+            Int64 sum = resultArr[0];
+            for (int i = 1; i < n; i++)
+            {
+                sum += resultArr[i];
+                if (max < sum)
+                    max = sum;
+            }
+            Console.WriteLine(max);
+            Console.ReadLine();
+        }
+    }
+}
