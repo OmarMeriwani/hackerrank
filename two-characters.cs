0b2f2c8e70cd4eca4e02def4bdeb79a37d309d22
Omar Meriwani <omar.sirwan@gmail.com>
2017-10-12 02:05:19 +0300

    --

---
+++ two-characters.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static string SelectDistinct (string original)
+    {
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+        for (int i = 0; i < original.Length; i++)
+        {
+            string c = original[i].ToString();
+            try
+            {
+                dic.Add(c,"1");
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+        string result = "";
+        foreach (KeyValuePair<string, string> k in dic)
+        {
+            result += k.Key;
+        }
+        return result;
+    }
+    static bool test(string s)
+    {
+        for (int i = 1 ; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static string GetOthers(string[] pair, string distinct)
+    {
+        string res = distinct;
+        foreach (string s in pair)
+        {
+            res = res.Replace(s, "");
+        }
+        return res;
+    }
+    static string RemoveOthers (string Others, string s)
+    {
+        string res = s;
+         for (int i = 0 ; i < Others.Length; i++)
+        {
+            string c = Others[i].ToString();
+            res = res.Replace(c,"");
+        }
+        return res;
+    }
+    static void Main(String[] args) 
+    {
+        int len = Convert.ToInt32(Console.ReadLine());
+        string s = Console.ReadLine();
+        string Distinct = SelectDistinct(s);
+        //Console.WriteLine(Distinct);
+        int Max = 0;
+        //Select Two Characters
+        for (int i = 0; i < Distinct.Length; i++)
+        {
+            for (int j = 0; j < Distinct.Length; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+                string Others = GetOthers(new string[] {Distinct[i].ToString(),Distinct[j].ToString() }, Distinct);
+                //Console.WriteLine("Pair:" + Distinct[i] + "," + Distinct[j] + " Others:" + Others);
+                string tt = RemoveOthers(Others, s);
+                //Console.WriteLine("After remove:" + tt + " from S:" + s);
+                bool b = test(tt);
+                //Console.WriteLine(b);
+                if (b == true)
+                {
+                    if (tt.Length > Max)
+                    {
+                        Max = tt.Length;
+                    }
+                }/**/
+            }
+        }
+        Console.WriteLine(Max.ToString());
+    }
+}
